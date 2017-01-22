9c9c10532de97001347a613276dd8b14cb7ff518
Sebastian Jachec <sjachec1@gmail.com>
2017-01-22 22:48:59 +0000

    More AWS tweaks

---
+++ aws/aws-setup.sh
@@ -0,0 +1,24 @@
+if ! test -f /usr/local/bin/pip
+then
+	wget https://bootstrap.pypa.io/get-pip.py
+	python3.6 get-pip.py
+fi
+
+echo "Installed pip, updated and upgraded"
+
+rm README.md
+rm LICENSE
+rm config/creds.template.py
+
+#Download creds
+cd config
+aws s3 cp s3://r-competitiveoverwatch/creds.py . --region us-east-1
+echo "Downloaded creds.py"
+
+#Go back to bot directory
+cd ../
+
+pip install -U -r requirements.txt
+echo "Installed Python dependencies"
+
+screen -S bot python3.6 bot.py
--- aws/aws-user-data.sh
+++ aws/aws-user-data.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+apt-get update -y
+apt-get install python3-pip -y
+
+#This is gross. `cloud-init` can't find `requests` by default (??)
+#Must be something I'm missing
+python3 -m pip install requests
+
 # Use Felix Krull's deadsnakes PPA.
 # python3.6 is in the universe repository for 16.10 not 16.04
 add-apt-repository ppa:fkrull/deadsnakes -y
-apt-get update -y
 apt-get install python3.6 -y
-apt-get install awscli -y
 apt-get upgrade -y
+apt-get dist-upgrade -y
 
 #Make user-data execute at boot
 #http://stackoverflow.com/questions/6475374/how-do-i-make-cloud-init-startup-scripts-run-every-time-my-ec2-instance-boots#comment60017981_10455027
@@ -14,17 +20,6 @@ sed -i 's/scripts-user$/\[scripts-user, always\]/' /etc/cloud/cloud.cfg
 echo "Set to execute user-data at instance boot"
 
 cd /home/ubuntu
-
-#Install pip as it does not appear to be installed with python for some reason
-if ! test -f get-pip.py
-then
-	wget https://bootstrap.pypa.io/get-pip.py
-	python3.6 get-pip.py
-fi
-
-echo "Installed pip, updated and upgraded"
-
-pip install --upgrade awscli -q
 
 #Update or clone
 if test -d bot
@@ -38,22 +33,7 @@ else
 	echo "Cloned bot"
 fi
 
-rm -rf AWS
-rm README.md
-rm LICENSE
-rm config/creds.template.py
+chmod +x aws-setup.sh
 
-#Download creds
-cd config
-aws s3 cp s3://r-competitiveoverwatch/creds.py . --region us-east-1
-echo "Downloaded creds.py"
-
-#Go back to bot directory
-cd ../
-
-python3.6 -m pip install -U -r requirements.txt -q
-echo "Installed Python dependencies"
-
-touch user-data-ran.test
-
-screen -S bot python3.6 bot.py
+echo "Executing setup"
+./aws/aws-setup.sh
